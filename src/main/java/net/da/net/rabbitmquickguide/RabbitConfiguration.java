a7ebc18c14f579ed5dc5811ba4052fc423bd067b
younguru <kiosun@mail.ru>
2022-08-04 16:48:28 +0300

    1

---
+++ src/main/java/net/da/net/rabbitmquickguide/RabbitConfiguration.java
@@ -0,0 +1,8 @@
+package net.da.net.rabbitmquickguide;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+}
---
+++ src/main/java/net/da/net/rabbitmquickguide/RabbitMQuickGuideApplication.java
@@ -0,0 +1,13 @@
+package net.da.net.rabbitmquickguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQuickGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQuickGuideApplication.class, args);
+    }
+
+}
